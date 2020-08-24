c731c4b5c7701546c99b8a0f167628e498d0e2e5
Austin Black <ablack@digitalocean.com>
2020-08-24 14:48:40 -0600

    add start of get tests, add error handling

--- app.py
+++ app.py
@@ -10,6 +10,7 @@ minblock = ""
 maxblock = ""
 numfiles = ""
 bucket = ""
+postresults = {}
 application = Flask(__name__)
 
 
@@ -18,13 +19,15 @@ def index():
     return render_template("index.html")
 
 
-@application.route("/testing", methods=['POST'])
-def testing():
+@application.route("/post-testing", methods=['POST'])
+def posttesting():
+
     # Form variables
     global minblock
     global maxblock
     global numfiles
     global bucket
+    global postresults
 
     minblock = request.form['minblock']
     maxblock = request.form['maxblock']
@@ -33,16 +36,24 @@ def testing():
     # timer storage
     time_to_upload = []
 
-    for x in range(numfiles):
-        command = "dd if=/dev/urandom of=/tmp/file.{} bs=$(shuf -i{}-{} -n1) count=1024".format(x, minblock, maxblock)
-        os.system(command)
+    try:
+        for x in range(numfiles):
+            command = "dd if=/dev/urandom of=/tmp/file.{} bs=$(shuf -i{}-{} -n1) count=1024".format(x, minblock, maxblock)
+            os.system(command)
 
-    session = boto3.session.Session()
-    client = session.client('s3',
-                            region_name=request.form['region'],
-                            endpoint_url= request.form['endpoint'],
-                            aws_access_key_id=request.form['accesskey'],
-                            aws_secret_access_key=request.form['secretkey'])
+    except Exception as error:
+        message = "Error generating objects, details potentially below:"
+        return render_template("error.html", error=error)
+    try:
+        session = boto3.session.Session()
+        client = session.client('s3',
+                                region_name=request.form['region'],
+                                endpoint_url= request.form['endpoint'],
+                                aws_access_key_id=request.form['accesskey'],
+                                aws_secret_access_key=request.form['secretkey'])
+    except Exception as error:
+        message = "Error initializing session, details potentially below:"
+        return render_template("error.html", error=error)
 
     total_start = time.time()
     for x in range(numfiles):
@@ -57,7 +68,38 @@ def testing():
     total_end = time.time()
     total_time = total_end - total_start
 
-    #cleanup logic goes here
+    # cleanup /tmp
+    os.system("rm -f /tmp/file.*")
+
+    mean = sum(time_to_upload) / int(numfiles)
+    lowest = min(time_to_upload)
+    highest = max(time_to_upload)
+
+    postresults['total'] = total_time
+    postresults['mean'] = mean
+    postresults['lowest'] = lowest
+    postresults['highest'] = highest
+
+
+    return render_template("partial-results.html", total_time=total_time, mean=mean, lowest=lowest, highest=highest)
+
+@application.route("/get-testing")
+def gettesting():
+
+    try:
+        session = boto3.session.Session()
+        client = session.client('s3',
+                                region_name=request.form['region'],
+                                endpoint_url= request.form['endpoint'],
+                                aws_access_key_id=request.form['accesskey'],
+                                aws_secret_access_key=request.form['secretkey'])
+    except Exception as error:
+        message = "Error initializing session, details potentially below:"
+        return render_template("error.html", error=error)
+
+    
+
+    # cleanup logic goes here
     filedict = [{"Key": f"file.{n}"} for n in range(numfiles)]
     response = client.delete_objects(
         Bucket=bucket,
@@ -67,13 +109,6 @@ def testing():
         },
     )
 
-    mean = sum(time_to_upload) / int(numfiles)
-    lowest = min(time_to_upload)
-    highest = max(time_to_upload)
-
-    # cleanup /tmp
-    os.system("rm -f /tmp/*")
-    return render_template("results.html", total_time=total_time, mean=mean, lowest=lowest, highest=highest)
 
 if __name__ == "__main__":
     application.run(host="0.0.0.0", port=80)
